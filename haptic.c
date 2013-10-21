804050563b02e9dbdb2cab064eea6d70d979ae48
barowley <brett.rowley.olin@gmail.com>
2013-10-21 17:06:42 -0400

    Skeleton code for haptic.c

--- haptic.c
+++ haptic.c
@@ -9,12 +9,65 @@
 #include "ui.h"
 #include <stdio.h>
 
+//DEFINE VENDOR REQUESTS
 #define HELLO       0   // Vendor request that prints "Hello World!"
 #define SET_VALS    1   // Vendor request that receives 2 unsigned integer values
 #define GET_VALS    2   // Vendor request that returns 2 unsigned integer values
-#define PRINT_VALS  3   // Vendor request that prints 2 unsigned integer values 
+#define PRINT_VALS  3   // Vendor request that prints 2 unsigned integer values
 
-uint16_t val1, val2;
+//DEFINE TIMERS TO BE USED
+#define LED_TIMER   &timer1
+#define PWM_TIMER   &timer2 
+
+//DEFINE PIN NAMES TO BE USED
+#define OUT1    &A[0]
+#define OUT2    &A[1]
+#define FB      &A[2]
+
+#define ENC     &D[0]
+#define SF      &D[1] 
+#define D2      &D[2] 
+#define D1      &D[3] 
+#define ENA     &D[4] 
+#define IN2     &D[5] 
+#define IN1     &D[6] 
+#define SLEW    &D[7] 
+#define INV     &D[8] 
+
+//DEFINE VARIABLES AND INITIAL VALUES
+void initChip(void);
+void initInt(void);
+void __attribute__((interrupt)) _CNInterrupt(void);
+
+uint16_t VAL1 = 1;
+uint16_t VAL2 = 0;
+
+//OTHER INITIAL CONDITIONS
+void initChip(){
+    init_clock();
+    init_uart();
+    init_timer();
+    init_ui();
+    init_pin();
+    init_oc();
+
+    pin_analogIn(OUT1);
+    pin_analogIn(OUT2);
+    pin_analogIn(FB);
+
+    pin_digitalOut(ENC);
+    pin_digitalOut(D1);
+    pin_digitalOut(D2);
+    pin_digitalOut(IN1);
+    pin_digitalOut(IN2);
+    pin_digitalOut(ENA);
+    pin_digitalOut(SLEW);
+    pin_digitalOut(INV);
+
+    pin_digitalIn(SF);
+    pin_digitalIn(ENC);
+}
+
 
 void VendorRequests(void) {
     WORD temp;
@@ -26,28 +79,28 @@ void VendorRequests(void) {
             BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
             break;
         case SET_VALS:
-            val1 = USB_setup.wValue.w;
-            val2 = USB_setup.wIndex.w;
+            VAL1 = USB_setup.wValue.w;
+            VAL2 = USB_setup.wIndex.w;
             BD[EP0IN].bytecount = 0;    // set EP0 IN byte count to 0 
             BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
             break;
         case GET_VALS:
-            temp.w = val1;
+            temp.w = VAL1;
             BD[EP0IN].address[0] = temp.b[0];
             BD[EP0IN].address[1] = temp.b[1];
-            temp.w = val2;
+            temp.w = VAL2;
             BD[EP0IN].address[2] = temp.b[0];
             BD[EP0IN].address[3] = temp.b[1];
             BD[EP0IN].bytecount = 4;    // set EP0 IN byte count to 4
             BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
             break;            
         case PRINT_VALS:
-            printf("val1 = %u, val2 = %u\n", val1, val2);
+            printf("VAL1 = %u, VAL2 = %u\n", VAL1, VAL2);
             BD[EP0IN].bytecount = 0;    // set EP0 IN byte count to 0
             BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
             break;
         default:
-            USB_error_flags |= 0x01;    // set Request Error Flag
+            USB_error_flags |= 0x01;
     }
 }
 
@@ -66,41 +119,38 @@ void VendorRequestsOut(void) {
 }
 
 int16_t main(void) {
-    init_clock();
-    init_uart();
-    init_timer();
-    init_ui();
-    init_pin();
-    init_oc();
+     InitUSB();                              // initialize the USB registers and serial interface engine
+     initChip();   
 
-     //starting conditions
     led_on(&led1);
-    timer_setPeriod(&timer2, 0.5); //start internal clock with defined period
-    timer_start(&timer2);
 
-    val1 = 0;
-    val2 = 0;
-    pin_digitalOut(&D[2]); //define servo output pins
-    pin_digitalOut(&D[3]);
-    oc_servo(&oc1, &(D[2]), &timer1, 20e-3f, 500e-6f, 2200e-6f, 0); 
-    oc_servo(&oc2, &(D[3]), &timer3, 20e-3f, 500e-6f, 2200e-6f, 0); /*call the oc_servo function as outlined in the course documentation packet
-            (specify which OC peripheral, use a timer, define the period of the control signal, define the min and max values of the PWM, initial position)
-            Values for the PWM signal were taken from servo spec sheet*/
+    pin_write(ENA, 1);  //sets ENABLE to HIGH
+    pin_write(IN1, 1);  
+    pin_write(IN2, 0);
+    pin_write(SLEW, 0);
+    pin_write(INV, 0);
+    pin_write(D1, 0);
+    pin_write(D2, 1);
 
+    timer_setPeriod(LED_TIMER, 0.5);         //start internal clock with defined period
+    timer_start(LED_TIMER);
 
-    InitUSB();                              // initialize the USB registers and serial interface engine
+    // oc_pwm(&oc1, D2, PWM_TIMER, 100, 0);
+
     while (USB_USWSTAT!=CONFIG_STATE) {     // while the peripheral is not configured...
         ServiceUSB();                       // ...service USB requests
     }
 
     while (1) {
         ServiceUSB();                       // service any pending USB requests
-        
-        if (timer_flag(&timer2)) { //starts timer
-            timer_lower(&timer2);
-            led_toggle(&led1); //toggles LED to confirm code has been written and is running
+
+        //LED1 TOGGLE TO CONFIRM RUNNING CODE
+        if (timer_flag(LED_TIMER)) {
+            timer_lower(LED_TIMER);
+            led_toggle(&led1);
+
+            pin_write(INV, led_read(&led1));
         }
-    pin_write(&D[2], val1); //writes val1 to servo 1
-    pin_write(&D[3], val2);	//writes val2 to servo 2
+        pin_write(D2, VAL1);        
     }
 }
--- human_ui.py
+++ human_ui.py
@@ -8,108 +8,37 @@ from time import sleep
 
 hello = hellousb.hellousb()
 
-KEY_W = chr(119) 		#define the keys to be used in the interface program
-KEY_A = chr(97)
+KEY_W = chr(119)
 KEY_S = chr(115)
+KEY_E = chr(101)
 KEY_D = chr(100)
-KEY_P = chr(112)
 
-# MAX = 2**16 - 1 		#put limits on servo motion
-MAX = 2**15 - 1 		#this is the range used for the test scan (~90 deg)
-MIN = 0
-FIDELITY = 20			#number of steps along each sweep, determines resolution of scan
-INC_VAL = MAX/FIDELITY 	#set the value to increment servo position by
-TILT_VAL = 30000 		#set initial values of PAN and TILT
-PAN_VAL = 30000			#in this case, the transducers will start straight up
+FREQ_INC = 10
+DUTY_INC = 1000
+VAL1 = 0 				#set variables to 0
+VAL2 = 0
 
-PING = 0 				#set variables to 0
-SCAN = 0
-convert = (1/526.4)		#conerts time of flight to distance based on calibration
+hello.set_vals(VAL1, VAL2)	#writes initial servo positions
 
-f=open('range_data2.txt', 'w')		#opens the text files used to store data, deletes existing data
-
-hello.set_vals(PAN_VAL, TILT_VAL)	#writes initial servo positions
-
-#INTRO TEXT
-print ('----------------\nReady to start! Use W,A,S,D keys to move, press P\nto see options for distance ranging.')
+#INTRO TEXT	
 print ('----------------\nBrett Rowley & Katherine Stegner - Olin College, 2013\n----------------')
 
 while True:
 	if msvcrt.kbhit():
 		key = msvcrt.getch()
 
-		#MANUAL CONTROLS
 		if key == KEY_W:
-			TILT_VAL += INC_VAL
+			VAL1 += FREQ_INC
+			print VAL1
 		elif key == KEY_S:
-			TILT_VAL -= INC_VAL
+			VAL1 -= FREQ_INC
+			print VAL1
+		elif key == KEY_E:
+			VAL2 += DUTY_INC
+			print VAL2
 		elif key == KEY_D:
-			PAN_VAL -= INC_VAL
-		elif key == KEY_A:
-			PAN_VAL += INC_VAL
-
-		#CONFIRMATION TO START AUTOMATED SWEEP
-		if key == KEY_P:
-			CONFIRM = raw_input("Automated Scan (1) or Single Ping (2)?")	
-			#asks user to choose between single ping or auto. sweep
-			#if auto, set pos to 0,0 and toggle SCAN																
-			if CONFIRM == '1':																		
-				PAN_VAL = 0
-				TILT_VAL = 0
-				hello.set_vals(PAN_VAL, TILT_VAL)		
-				SCAN =not SCAN 						#toggle SCAN variable (see below)
-				print 'ULTRASONIC MAPPING STARTED'
-				print 'Hit Ctrl+C to abort'
-			#if single, run PING once
-			elif CONFIRM == '2':
-				ping = hello.get_ping()				#call GET_PING once
-				ping = float(ping[0])				#store the time of flight value as ping
-				dist = (ping - 23313) * convert		#conert from time to distance
-				print ping 							#print both values for operator to see
-				print dist
-			#if neither, print Cancelled
-			elif CONFIRM != '1' or CONFIRM != '2':
-				print 'Cancelled'
-	
-	#AUTOMATED SWEEP
-	if SCAN == True:
-		while (PAN_VAL < MAX):			#while number of pan steps is lower than desired number
-			hello.set_vals(PAN_VAL, TILT_VAL)
-
-			ping = hello.get_ping()		#calls ping, stores values in variables
-			ping = float(ping[0])
-			dist = (ping -23313) * convert
-			print ping
-			
-			PAN_DEG = round((PAN_VAL*(0.0027466)), 2)	#converts servo positions to degrees, stores in variables
-			TILT_DEG = round((TILT_VAL*(0.0027466)), 2)
-
-			f=open('range_data.txt', 'a')	#opens text file in append mode to add data in new lines
-			f.write(repr(PAN_DEG) + ', ' + repr(TILT_DEG) + ', ' + repr(dist) + '\n')	#write servo positions and measured distance to file
-
-			sleep(0.5)	#wait for file to finish writing
-
-			PAN_VAL += INC_VAL		#increment pan position
-
-		else:						#once pan sweep complete, reset and increment tilt
-			PAN_VAL = MIN
-			TILT_VAL += INC_VAL
-			if TILT_VAL > MAX:
-				print ('Sweep Completed, consult range_plot.m for results')
-				SCAN =not SCAN
-				
-
-	#BUFFER TO PREVENT OVERRUNING SERVOS
-	if PAN_VAL > MAX:
-		PAN_VAL = MAX
-	if PAN_VAL < MIN:
-		PAN_VAL = MIN
-	if TILT_VAL > MAX:
-		TILT_VAL = MAX
-	if TILT_VAL < MIN:
-		TILT_VAL = MIN
-
-	hello.set_vals(PAN_VAL, TILT_VAL)
-	# print hello.get_ping()
+			VAL2 -= DUTY_INC
+			print VAL2
+	hello.set_vals(VAL1, VAL2)
 
 

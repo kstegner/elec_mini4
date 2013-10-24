53ff6e5383bb9ea69d4bf6ea021b8ceb581f1fad
barowley <brett.rowley.olin@gmail.com>
2013-10-24 15:13:40 -0400

    Enabled PWM control of motor

--- haptic.c
+++ haptic.c
@@ -10,36 +10,34 @@
 #include <stdio.h>
 
 //DEFINE VENDOR REQUESTS
-#define HELLO       0   // Vendor request that prints "Hello World!"
 #define SET_VALS    1   // Vendor request that receives 2 unsigned integer values
 #define GET_VALS    2   // Vendor request that returns 2 unsigned integer values
-#define PRINT_VALS  3   // Vendor request that prints 2 unsigned integer values
 
-//DEFINE TIMERS TO BE USED
-#define LED_TIMER   &timer1
-#define PWM_TIMER   &timer2 
+//DEFINE TIMERS
+#define LED_TIMER   &timer1     //timer for LEDs
+#define PWM_TIMER   &timer2     //timer for PWM controller
 
-//DEFINE PIN NAMES TO BE USED
-#define OUT1    &A[0]
-#define OUT2    &A[1]
+//DEFINE PIN NAMES
+#define CUR     &A[0]   //OUT1 - Current pin
+#define EMF     &A[1]   //OUT2 - Back EMF pin
 #define FB      &A[2]
 
-#define ENC     &D[0]
-#define SF      &D[1] 
-#define D2      &D[2] 
-#define D1      &D[3] 
-#define ENA     &D[4] 
-#define IN2     &D[5] 
-#define IN1     &D[6] 
-#define SLEW    &D[7] 
-#define INV     &D[8] 
+#define ENC     &D[0]   //Encoder pin
+#define SF      &D[1]   //Status Flag pin
+#define D2      &D[2]   //Disable IN2 pin
+#define D1      &D[3]   //Disable IN1 pin
+#define ENA     &D[4]   //Enable motor pin
+#define IN2     &D[5]   //Input 2 pin
+#define IN1     &D[6]   //Input 1 pin
+#define SLEW    &D[7]   //Slew rate pin
+#define INV     &D[8]   //Inverting pin
 
 //DEFINE VARIABLES AND INITIAL VALUES
 void initChip(void);
 void initInt(void);
 void __attribute__((interrupt)) _CNInterrupt(void);
 
-uint16_t VAL1 = 1;
+uint16_t VAL1 = 65536*2/5;
 uint16_t VAL2 = 0;
 
 //OTHER INITIAL CONDITIONS
@@ -51,11 +49,13 @@ void initChip(){
     init_pin();
     init_oc();
 
-    pin_analogIn(OUT1);
-    pin_analogIn(OUT2);
+    pin_analogIn(CUR);
+    pin_analogIn(EMF);
     pin_analogIn(FB);
 
-    pin_digitalOut(ENC);
+    pin_digitalIn(SF);
+    pin_digitalIn(ENC);
+
     pin_digitalOut(D1);
     pin_digitalOut(D2);
     pin_digitalOut(IN1);
@@ -64,26 +64,29 @@ void initChip(){
     pin_digitalOut(SLEW);
     pin_digitalOut(INV);
 
-    pin_digitalIn(SF);
-    pin_digitalIn(ENC);
+    oc_pwm(&oc1, D2, PWM_TIMER, 250, 0); 
 }
-
+//INITIALIZE MOTOR
+void initMotor(void) {
+    pin_write(ENA, 1);  //set enable pin to ON
+    pin_write(SLEW, 1); //set slew rate to HIGH
+    pin_write(INV, 0);  //set invert OFF
+    pin_write(D1, 0);   //disable IN1 is OFF
+    pin_write(D2, 1);   //disable IN2 is ON
+}
 
 void VendorRequests(void) {
     WORD temp;
 
     switch (USB_setup.bRequest) {
-        case HELLO:
-            printf("Hello World!\n");
-            BD[EP0IN].bytecount = 0;    // set EP0 IN byte count to 0 
-            BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
-            break;
+
         case SET_VALS:
             VAL1 = USB_setup.wValue.w;
             VAL2 = USB_setup.wIndex.w;
             BD[EP0IN].bytecount = 0;    // set EP0 IN byte count to 0 
             BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
             break;
+
         case GET_VALS:
             temp.w = VAL1;
             BD[EP0IN].address[0] = temp.b[0];
@@ -93,12 +96,8 @@ void VendorRequests(void) {
             BD[EP0IN].address[3] = temp.b[1];
             BD[EP0IN].bytecount = 4;    // set EP0 IN byte count to 4
             BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
-            break;            
-        case PRINT_VALS:
-            printf("VAL1 = %u, VAL2 = %u\n", VAL1, VAL2);
-            BD[EP0IN].bytecount = 0;    // set EP0 IN byte count to 0
-            BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
-            break;
+            break;   
+
         default:
             USB_error_flags |= 0x01;
     }
@@ -110,7 +109,6 @@ void VendorRequestsIn(void) {
             USB_error_flags |= 0x01;                    // set Request Error Flag
     }
 }
-
 void VendorRequestsOut(void) {
     switch (USB_request.setup.bRequest) {
         default:
@@ -120,22 +118,16 @@ void VendorRequestsOut(void) {
 
 int16_t main(void) {
      InitUSB();                              // initialize the USB registers and serial interface engine
-     initChip();   
+     initChip();
+     initMotor();   
 
     led_on(&led1);
-
-    pin_write(ENA, 1);  //sets ENABLE to HIGH
-    pin_write(IN1, 1);  
-    pin_write(IN2, 0);
-    pin_write(SLEW, 0);
-    pin_write(INV, 0);
-    pin_write(D1, 0);
-    pin_write(D2, 1);
-
     timer_setPeriod(LED_TIMER, 0.5);         //start internal clock with defined period
     timer_start(LED_TIMER);
 
-    // oc_pwm(&oc1, D2, PWM_TIMER, 100, 0);
+    pin_write(IN1, 1);
+    pin_write(IN2, 0);
+    pin_write(D2, 65536*2/5);
 
     while (USB_USWSTAT!=CONFIG_STATE) {     // while the peripheral is not configured...
         ServiceUSB();                       // ...service USB requests
@@ -149,8 +141,7 @@ int16_t main(void) {
             timer_lower(LED_TIMER);
             led_toggle(&led1);
 
-            pin_write(INV, led_read(&led1));
-        }
-        pin_write(D2, VAL1);        
+            pin_write(D2, VAL1);
+        }       
     }
 }
--- human_ui.py
+++ human_ui.py
@@ -13,12 +13,10 @@ KEY_S = chr(115)
 KEY_E = chr(101)
 KEY_D = chr(100)
 
-FREQ_INC = 10
+FREQ_INC = 1
 DUTY_INC = 1000
 VAL1 = 0 				#set variables to 0
 VAL2 = 0
-
-hello.set_vals(VAL1, VAL2)	#writes initial servo positions
 
 #INTRO TEXT	
 print ('----------------\nBrett Rowley & Katherine Stegner - Olin College, 2013\n----------------')
@@ -28,17 +26,15 @@ while True:
 		key = msvcrt.getch()
 
 		if key == KEY_W:
-			VAL1 += FREQ_INC
-			print VAL1
+			VAL1 += DUTY_INC
 		elif key == KEY_S:
-			VAL1 -= FREQ_INC
-			print VAL1
-		elif key == KEY_E:
-			VAL2 += DUTY_INC
-			print VAL2
-		elif key == KEY_D:
-			VAL2 -= DUTY_INC
-			print VAL2
+			VAL1 -= DUTY_INC
+
+	if VAL1 <= 0:
+		VAL1 = 0
+	elif VAL1 >= 65536:
+		VAL1 = 65500
+
 	hello.set_vals(VAL1, VAL2)
 
 
